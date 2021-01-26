20cdb120ee1501ebf9740d5355db7f969b8aeee0
lyrieek <tianhao7@foxmail.com>
2021-01-26 23:55:12 +0800

    :toda:init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GitList />
+  </div>
+</template>
+
+<script>
+import GitList from './components/GitList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GitList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GitList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Git</h1>
+    <ul>
+      <li>dir</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GitList'
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
