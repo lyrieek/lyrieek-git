9d63d18adf72e21461fce02d279006d786db03a8
lyrieek <lyrieek@qq.com>
2021-02-15 13:02:05 +0800

    :lipstick: improve commit date tip message

--- src/components/GitCommit.vue
+++ src/components/GitCommit.vue
@@ -5,13 +5,19 @@
 			<DatePicker v-model="commitInfo.date" type="date" placeholder="Commit date" style="width: 120px"></DatePicker>
 			<TimePicker v-model="commitInfo.time" format="HH:mm:ss" placeholder="Commit time" style="width: 120px"></TimePicker>
 			<Input v-model="commitInfo.zone" placeholder="Time zone" style="width: 85px" />
-			<Button shape="circle" icon="md-refresh" @click="commitInfoUpdate()" :disabled="commitInfo.pin" style="margin-left: 2px"></Button>
+			<Tooltip placement="bottom">
+				<Icon type="ios-information-circle-outline" style="vertical-align: bottom; margin-bottom: -5px;" />
+				<div slot="content">
+					<span>{{ getCommitShortDate() }}</span>
+				</div>
+			</Tooltip>
+			<Button shape="circle" icon="md-refresh" @click="commitInfoUpdate()" style="margin-left: 2px"></Button>
 			<Tooltip placement="bottom">
 				<CheckboxGroup>
 					<Checkbox label="Pin" border style="margin-left: 2px;padding: 0px 0px 0px 5px;margin-right:0px" v-model="commitInfo.pin"></Checkbox>
 				</CheckboxGroup>
 				<div slot="content">
-					<Icon type="ios-outlet-outline" />Synchronize real time
+					<Icon type="ios-outlet-outline" />Automatically synchronize time after writing commit message
 				</div>
 			</Tooltip>
 		</div>
@@ -59,12 +65,18 @@ export default {
 		refreshStatus() {
 			this.$root.$emit("refreshStatus")
 		},
+		getCommitShortDate() {
+			if (!this.commitInfo.date) {
+				return 'Not filled'
+			}
+			return moment(this.commitInfo.date).format('YYYY-MM-DD') + "T" + this.commitInfo.time + this.commitInfo.zone
+		},
 		commit() {
 			fetch("http://localhost:3516/commit", {
 				method: 'POST',
 				body: JSON.stringify({
 					message: this.commitInfo.message,
-					date: moment(this.commitInfo.date).format('YYYY-MM-DD') + "T" + this.commitInfo.time + this.commitInfo.zone,
+					date: this.getCommitShortDate(),
 					gpg: this.CommitConfig.GPGEnable,
 					signOff: this.CommitConfig.SignedOffEnable
 				})
