f441ffa1a264e8211166231d33100448f5377e69
lyrieek <lyrieek@qq.com>
2021-02-15 16:23:00 +0800

    :construction: adjust data struct & add clearCommitInfo function
    Signed-off-by: lyrieek <lyrieek@qq.com>

--- src/components/GitCommit.vue
+++ src/components/GitCommit.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<Input v-model="commitInfo.message" maxlength="100" @on-blur="commitInfoUpdate()" @on-focus="refreshStatus()" :rows=3 show-word-limit type="textarea" placeholder="Commit message" style="width: 100%" />
+		<Input v-model="message" maxlength="100" @on-blur="commitInfoUpdate()" @on-focus="refreshStatus()" :rows=3 show-word-limit type="textarea" placeholder="Commit message" style="width: 100%" />
 		<div style="text-align: right">
-			<DatePicker v-model="commitInfo.date" type="date" placeholder="Commit date" style="width: 120px"></DatePicker>
-			<TimePicker v-model="commitInfo.time" format="HH:mm:ss" placeholder="Commit time" style="width: 120px"></TimePicker>
-			<Input v-model="commitInfo.zone" placeholder="Time zone" style="width: 85px" />
+			<DatePicker v-model="date" type="date" placeholder="Commit date" style="width: 120px"></DatePicker>
+			<TimePicker v-model="time" format="HH:mm:ss" placeholder="Commit time" style="width: 120px"></TimePicker>
+			<Input v-model="zone" placeholder="Time zone" style="width: 85px" />
 			<Tooltip placement="bottom">
 				<Icon type="ios-information-circle-outline" style="vertical-align: bottom; margin-bottom: -5px;" />
 				<div slot="content">
@@ -14,12 +14,14 @@
 			<Button shape="circle" icon="md-refresh" @click="commitInfoUpdate()" style="margin-left: 2px"></Button>
 			<Tooltip placement="bottom">
 				<CheckboxGroup>
-					<Checkbox label="Pin" border style="margin-left: 2px;padding: 0px 0px 0px 5px;margin-right:0px" v-model="commitInfo.pin"></Checkbox>
+					<Checkbox label="Pin" border style="margin: 0px 5px;padding: 0px 0px 0px 5px;" v-model="pin"></Checkbox>
 				</CheckboxGroup>
 				<div slot="content">
 					<Icon type="ios-outlet-outline" />Automatically synchronize time after writing commit message
 				</div>
 			</Tooltip>
+			<Button @click="clearCommitInfo()">
+				<Icon type="ios-trash" />Clear</Button>
 		</div>
 		<div style="margin-top: 10px; text-align: right">
 			<Button type="success" @click="commit()">
@@ -34,51 +36,54 @@ import moment from 'moment'
 export default {
 	name: "GitCommit",
 	data: () => ({
-		commitInfo: {
-			message: "",
-			date: null,
-			time: null,
-			zone: null,
-			pin: false
-		},
-		CommitConfig: {
-			GPGEnable: false,
-			SignedOffEnable: false
-		}
+		message: "",
+		date: null,
+		time: null,
+		zone: null,
+		pin: false,
+		GPGEnable: true,
+		SignedOffEnable: true
 	}),
 	created() {
 		this.$root.$on("updateCommitConfig", (e) => {
-			this.CommitConfig.GPGEnable = e.GPGEnable
-			this.CommitConfig.SignedOffEnable = e.SignedOffEnable
+			this.GPGEnable = e.GPGEnable
+			this.SignedOffEnable = e.SignedOffEnable
 		})
 	},
 	methods: {
 		commitInfoUpdate() {
-			if (this.commitInfo.pin || !this.commitInfo.message) {
+			if (this.pin || !this.message) {
 				return
 			}
-			this.commitInfo.date = new Date()
-			this.commitInfo.time = moment().format('HH:mm:ss')
-			this.commitInfo.zone = "+0800"
-			this.commitInfo.pin = true
+			this.date = new Date()
+			this.time = moment().format('HH:mm:ss')
+			this.zone = "+0800"
+			this.pin = true
+		},
+		clearCommitInfo() {
+			this.message = ""
+			this.date = null
+			this.time = null
+			this.zone = ""
+			this.pin = false
 		},
 		refreshStatus() {
 			this.$root.$emit("refreshStatus")
 		},
 		getCommitShortDate() {
-			if (!this.commitInfo.date) {
+			if (!this.date) {
 				return 'Not filled'
 			}
-			return moment(this.commitInfo.date).format('YYYY-MM-DD') + "T" + this.commitInfo.time + this.commitInfo.zone
+			return moment(this.date).format('YYYY-MM-DD') + "T" + this.time + this.zone
 		},
 		commit() {
 			fetch("http://localhost:3516/commit", {
 				method: 'POST',
 				body: JSON.stringify({
-					message: this.commitInfo.message,
+					message: this.message,
 					date: this.getCommitShortDate(),
-					gpg: this.CommitConfig.GPGEnable,
-					signOff: this.CommitConfig.SignedOffEnable
+					gpg: this.GPGEnable,
+					signOff: this.SignedOffEnable
 				})
 			}).then((e) => {
 				if (!e.ok) {
@@ -96,7 +101,7 @@ export default {
 						title: 'Commit',
 						desc: res
 					})
-					this.commitInfo = {}
+					this.clearCommitInfo()
 				})
 			})
 		}
