691d969bae8ca4123ca58add9e387c1889b7b05f
lyrieek <lyrieek@qq.com>
2021-03-09 20:35:34 +0800

    :rocket: add shebang prefix
    Signed-off-by: lyrieek <lyrieek@qq.com>

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -2,13 +2,15 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json'
+import shebang from 'rollup-plugin-add-shebang';
 
+const _outputFile = '../dist/index.js'
 
 export default {
 	input: 'index.ts',
 	output: {
-		file: '../dist/index.js',
+		file: _outputFile,
 		format: 'cjs'
 	},
-	plugins: [nodeResolve({ preferBuiltins: true }), commonjs(), typescript(), json()]
+	plugins: [nodeResolve({ preferBuiltins: true }), commonjs(), shebang({include: _outputFile}), typescript(), json()]
 }
