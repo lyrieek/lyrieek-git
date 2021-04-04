136c2e18ad08f7b2c5d67bb71f1f75fdc0e559e1
lyrieek <lyrieek@qq.com>
2021-04-04 15:53:21 +0800

    :sparkles: revise menu header (add about view)

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,40 @@
 	<div class="layout">
 		<Layout>
 			<Header :style="{position: 'fixed', width: '100%', 'z-index': 999, top: 0}">
-				<Menu mode="horizontal" theme="dark" active-name="1">
+				<Menu mode="horizontal" theme="dark" active-name="1" @on-select="about">
 					<div class="layout-logo">
 						&nbsp;
 						<Icon type="md-build" />
 						Lyrieek Git
 					</div>
-					<div class="layout-nav">
+					<div class="layout-nav" style="margin-right: 0px;">
 						<MenuItem name="1">
-						<Icon type="ios-navigate"></Icon>
-						刷新
-						</MenuItem>
-						<MenuItem name="2">
-						<Icon type="ios-keypad"></Icon>
-						更换项目
-						</MenuItem>
-						<MenuItem name="4">
-						<Icon type="ios-paper"></Icon>
-						提取报告
+						关于
 						</MenuItem>
 					</div>
 				</Menu>
 			</Header>
-			<Content :style="{margin: '88px 20px 0', background: '#fff', 'min-height': '500px'}">
+			<Content :style="{margin: '88px 20px 0', 'min-height': '500px'}">
 				<GitList />
 			</Content>
 			<Footer class="layout-footer-center">2021 &copy; Lyrieek</Footer>
 		</Layout>
+		<Modal title="About" v-model="aboutWinVisible">
+			<h3>Lyrieek-Git</h3>
+			<ul>
+				<li>版本: <em>v0.1.2-Alpha</em></li>
+				<li>作者: <em>lyrieek</em></li>
+				<li>邮箱: <em>lyrieek@qq.com</em></li>
+				<li>许可: <em>AGPL(GNU AFFERO GENERAL PUBLIC LICENSE)</em></li>
+				<li style="text-align: right;">Copyleft Ɔ 2021 lyrieek</li>
+			</ul>
+		</Modal>
 	</div>
 </template>
 <style scoped>
 .layout {
 	border: 1px solid #d7dde4;
-	/* background: #f5f7f9; */
+	background: #fefefe;
 	position: relative;
 	border-radius: 4px;
 	overflow: hidden;
@@ -70,11 +71,17 @@ import GitList from './components/GitList.vue'
 
 export default {
 	name: 'App',
-	data: () => ({
-		maxHeight: window.innerHeight - 95
-	}),
 	components: {
 		GitList
+	},
+	data: () => ({
+		maxHeight: window.innerHeight - 95,
+		aboutWinVisible: false
+	}),
+	methods: {
+		about() {
+			this.aboutWinVisible = true
+		}
 	}
 }
 
